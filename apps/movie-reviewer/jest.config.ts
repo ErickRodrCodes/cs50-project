da39876f39058e2fb8ffeacc7160258e879a9c4e
Erick Rodriguez <tbogard@outlook.com>
2023-02-15 22:45:16 -0500

    Initial commit

---
+++ apps/movie-reviewer/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'movie-reviewer',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/movie-reviewer',
+};
